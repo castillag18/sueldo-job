a2249b8d78da2d34914489da9a026bd35c1ac561
unknown <orlandojs199918@gmail.com>
2018-03-15 08:54:44 -0500

    sueldo

--- src/sueldo/Sueldo.java
+++ src/sueldo/Sueldo.java
@@ -1,32 +1,18 @@
 package sueldo;
 
-
 import javax.swing.JOptionPane;
 import sueldo.horas_trabaja;
 
 public class Sueldo {
 
     public static void main(String[] args) {
-        
-        horas_trabaja h1 = new horas_trabaja ();
-        
-       double sueldos;
-       int horas;
-       double pagoh;
-       
-       horas = Integer.parseInt(JOptionPane.showInputDialog("diga las horas trabajadas"));
-       pagoh = Double.parseDouble(JOptionPane.showInputDialog("diga el valor de la hora"));
-       
-       if(h1.getHoras_trabaja() < 40 ){
-          horas = (int) (horas * pagoh * 5) ;
-          System.out.println("el saldo a pagar es : " + horas);
-       }else
-           if(h1.getPago_x_horas() >= 40){
-             horas = (int)(horas * Math.pow(pagoh,2)) * 5 ; 
-             sueldos = h1.getPago_semanal() + horas ;
-             System.out.println("el saldo a pagar es : " + sueldos);
-           }
+
+        horas_trabaja h1 = new horas_trabaja();
+
+        h1.setHoras_trabaja(Integer.parseInt(JOptionPane.showInputDialog("diga las horas trabajadas")));
+        h1.setPago_x_horas(Double.parseDouble(JOptionPane.showInputDialog("diga el valor de la hora")));
+        h1.getPagoSemanal();
         
     }
-    
+
 }
--- src/sueldo/horas_trabaja.java
+++ src/sueldo/horas_trabaja.java
@@ -10,15 +10,15 @@ package sueldo;
  * @author orlando
  */
 public class horas_trabaja {
-    
+
     private double pago_semanal;
     private int horas_trabaja;
     private double pago_x_horas;
 
-    public horas_trabaja(double pago_semanal, int horas_trabaja, double pago_x_horas) {
+    public horas_trabaja(double pago_semanal, int horas_trabaja) {
         this.pago_semanal = pago_semanal;
         this.horas_trabaja = horas_trabaja;
-        this.pago_x_horas = pago_x_horas;
+
     }
 
     public horas_trabaja() {
@@ -47,6 +47,18 @@ public class horas_trabaja {
     public void setPago_x_horas(double pago_x_horas) {
         this.pago_x_horas = pago_x_horas;
     }
-    
-    
+
+    public void getPagoSemanal() {
+
+        if (horas_trabaja < 40) {
+            pago_semanal = (horas_trabaja * pago_x_horas);
+            System.out.println("el saldo a pagar es : " + pago_semanal);
+        } else if (horas_trabaja >= 40) {
+            pago_semanal = 40 * pago_x_horas;
+            pago_semanal += ((horas_trabaja - 40) * pago_x_horas) * 2;
+            //sueldos = h1.getPago_semanal() + horas ;             
+            System.out.println("el saldo de la semana a pagar es : " + pago_semanal);
+        }
+    }
+
 }
